bc4162a9db081c32edd0a704b32179a9a67125aa
Jose Moreno <58490017+saterunholy@users.noreply.github.com>
2020-08-28 13:46:23 -0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,37 @@
-export const funcParams = 'data, theme, echartsInstance, echarts';
+export const funcParams = "data, theme, echartsInstance, echarts";
 
 const funcBody = `const series = data.series.map((s) => {
   const sData = s.fields.find((f) => f.type === 'number').values.buffer;
   const sTime = s.fields.find((f) => f.type === 'time').values.buffer;
 
   return {
-    name: s.name,
-    type: 'pie',
-    roseType: 'radius',
-    radius: [40, 90],
-    center: ['50%', '50%'],
-    showSymbol: true,
-    areaStyle: {
-      opacity: 0.1,
-    },
-    lineStyle: {
-      width: 1,
-    },
+    name: 'pie2',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            roseType: 'radius',
+            label: {
+                color: 'rgba(255, 255, 255, 0.3)'
+            },
+            labelLine: {
+                lineStyle: {
+                    color: 'rgba(255, 255, 255, 0.3)'
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+            },
+            itemStyle: {
+                color: '#2DDA2C',
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+            },
+
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+                return Math.random() * 200;
+            },
     data: sData.map((d, i) => [sTime[i], d.toFixed(2)]),
   };
 });
@@ -51,6 +66,23 @@ return {
       color: 'rgba(128, 128, 128, .9)',
     },
   },
+
+  graphic: {
+    type: 'image',
+            id: 'logo',
+            right: 20,
+            top: 10,
+            z: -10,
+            bounding: 'raw',
+            origin: [75, 75],
+            style: {
+                image: 'https://i.ibb.co/5r1m5Q2/1-removebg-preview.png',
+                width: 100,
+                height: 100,
+                opacity: 1
+            }
+  },
+  
   xAxis: Object.assign(
     {
       type: 'time',
@@ -82,11 +114,11 @@ return {
 // const funcBody = matchResult ? matchResult[1] : '';
 
 export interface SimpleOptions {
-  followTheme: boolean,
+  followTheme: boolean;
   getOption: string;
 }
 
 export const defaults: SimpleOptions = {
   followTheme: false,
-  getOption: funcBody,
+  getOption: funcBody
 };
